b97f31f2dccf877c8935f1dc90234f1ce9f12065
Joel Foubia <joelngamba47@gmail.com>
2023-07-24 16:07:07 +0100

    add snap functionality

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-snap-face',
   templateUrl: './snap-face.component.html',
   styleUrls: ['./snap-face.component.scss']
 })
-export class SnapFaceComponent {
+export class SnapFaceComponent implements OnInit{
+  title! : string;
+  description! : string;
+  createdDate! : Date;
+  snaps! : number;
+  imageUrl! : string;
 
+  ngOnInit(): void {
+    this.title = "FaceSnap";
+    this.description = "This is my first description for face snaps";
+    this.createdDate = new Date;
+    this.snaps = 6;
+    this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
+  }
+
+  onFaceSnap(){
+    this.snaps++;
+  }
 }
