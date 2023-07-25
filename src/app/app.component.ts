8c6b67b63ac77cd08ec7a1d77c99e9d0eb5c4c37
Joel Foubia <joelngamba47@gmail.com>
2023-07-25 01:51:26 +0100

    Reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { FaceSnap } from './models/face-snap.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-   
+export class AppComponent implements OnInit{
+   mySnap! : FaceSnap;
+   middleSnap! : FaceSnap;
+   lastSnap! : FaceSnap;
+   ngOnInit(){
+     this.mySnap = new FaceSnap(
+       'Archibald',
+       'Mon meilleiur ami depuis tout petit !',
+       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+       new Date(),
+      0
+     );
+      this.middleSnap = new FaceSnap(
+       'Archibald',
+       'Mon meilleiur ami depuis tout petit !',
+       'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+       new Date(),
+      0
+     );
+   }
 }
--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.models';
 
 @Component({
   selector: 'app-snap-face',
@@ -6,21 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./snap-face.component.scss']
 })
 export class SnapFaceComponent implements OnInit{
+  @Input() faceSnap! : FaceSnap;
+
   title! : string;
   description! : string;
   createdDate! : Date;
   snaps! : number;
   imageUrl! : string;
+  buttonText! : string;
 
   ngOnInit(): void {
     this.title = "FaceSnap";
     this.description = "This is my first description for face snaps";
     this.createdDate = new Date;
     this.snaps = 6;
+    this.buttonText = "Oh snap!";
     this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
   }
 
   onFaceSnap(){
-    this.snaps++;
+    if(this.buttonText === 'Oh snap!'){
+      this.faceSnap.snaps++;
+      this.buttonText = "Oops, unsnap!";
+    }else{
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh snap!";
+    }
   }
 }
