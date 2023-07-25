35238a026f621c009cc19b08140d8a0389df4c79
Joel Foubia <joelngamba47@gmail.com>
2023-07-25 20:57:43 +0100

    changement de la stucture de contructeur a simple declaration

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -9,20 +9,10 @@ import { FaceSnap } from '../models/face-snap.models';
 export class SnapFaceComponent implements OnInit{
   @Input() faceSnap! : FaceSnap;
 
-  title! : string;
-  description! : string;
-  createdDate! : Date;
-  snaps! : number;
-  imageUrl! : string;
   buttonText! : string;
 
   ngOnInit(): void {
-    this.title = "FaceSnap";
-    this.description = "This is my first description for face snaps";
-    this.createdDate = new Date;
-    this.snaps = 6;
     this.buttonText = "Oh snap!";
-    this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
   }
 
   onFaceSnap(){
